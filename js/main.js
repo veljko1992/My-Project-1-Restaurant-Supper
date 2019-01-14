2c98d37fe03f285fb54cbb34d6636d10147945b9
veljko.1992 <veljko.1992@gmail.com>
2019-01-14 18:20:58 +0100

    Work in progress

--- js/main.js
+++ js/main.js
@@ -113,3 +113,23 @@ $(document).ready(function () {
     $("#theDate").attr("value", today);
 });
 //Pushing a curent date in input date for reservation END
+
+//Gallery zoom
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function (item) {
+                return item.el.attr('title') + '<small>by Veljko Ivanovic</small>';
+            }
+        }
+    });
+//Gallery zoom END
