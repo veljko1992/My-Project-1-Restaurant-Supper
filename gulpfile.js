3d83e1087539a3f49b41659976262996e5dadae1
veljko.1992 <veljko.1992@gmail.com>
2019-01-15 12:29:06 +0100

    Work in progress

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
     return gulp.src('sass/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('css'));
 });
 
