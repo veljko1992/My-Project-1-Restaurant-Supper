3bd4327b60152a1b2c29fd98c2f173a7408a99c2
veljko.1992 <veljko.1992@gmail.com>
2019-01-17 11:38:29 +0100

    Work in progress

--- js/main.js
+++ js/main.js
@@ -142,7 +142,10 @@ var $popupGallery = $('.popup-gallery');
     var galleryImgs = result;
     for (i = 0; i < galleryImgs.length; i++) {
         $popupGallery.append(`<div class="galleryItem col-12 col-sm-6 col-lg-4">
-                        <a href="img/gallery1.jpg" title="Events"><img src="img/${galleryImgs[i].name}" alt="RestaurantImg" class="img-res"></a>
+                        <a href = "img/${galleryImgs[i].name}"
+                        title = "Events" > <img src = "img/${galleryImgs[i].name}"
+                        alt = "RestaurantImg"
+                        class = "img-res"> </a>
                         </div>`);
     }
 });
